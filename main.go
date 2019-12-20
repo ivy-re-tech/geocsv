642d367aa01b5ad9fbf811d6d52f30852afed8bd
Myles Haynes <myleschaynes@gmail.com>
2019-12-20 19:08:34 +1300

    Add some testdata, and goreleaser

--- main.go
+++ main.go
@@ -2,35 +2,51 @@ package main
 
 import (
 	"encoding/csv"
+	"fmt"
 	"github.com/alecthomas/kingpin"
 	"github.com/paulmach/orb"
 	"github.com/paulmach/orb/encoding/wkt"
 	"io"
 	"log"
 	"os"
+	"os/signal"
 	"strconv"
+	"syscall"
 )
 
 var (
 	in     = kingpin.Arg("input", "The input file to use, if not specified, stdin is used").File()
-	out    = kingpin.Arg("out", "The output file to use, if not specified, stdout is used").File()
+	out    = kingpin.Arg("out", "The output file to use, if not specified, stdout is used").String()
 	h      = kingpin.Flag("header", "Whether or not to skip the first row or not").Bool()
 	latIdx = kingpin.Flag("lat", "The column index of the latitude column").Required().Int()
 	lngIdx = kingpin.Flag("lng", "The column index of the longitude column").Required().Int()
 	delim  = kingpin.Flag("d", "The delimiter to use for CSV files").Default(",").String()
+	name   = kingpin.Flag("name", "Column name for WKT").Default("WKT").String()
 )
 
 func main() {
 	kingpin.Parse()
-	var input io.Reader
+	err := run()
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func run() error {
+	var input io.ReadCloser
 	if in != nil && *in != nil {
 		input = *in
 	} else {
 		input = os.Stdin
 	}
+	go closeOnEnd(input)
 	var output io.WriteCloser
-	if out != nil && *out != nil {
-		output = *out
+	if out != nil && len(*out) > 0{
+		f, err := os.OpenFile(*out, os.O_CREATE, os.ModeAppend)
+		if err != nil {
+			return err
+		}
+		output = f
 	} else {
 		output = os.Stdout
 	}
@@ -41,9 +57,18 @@ func main() {
 
 	var row int
 	if *h {
-		_, err := reader.Read()
+		header, err := reader.Read()
 		if err != nil {
-			log.Fatalf("failed to read CSV: %s\n", err)
+			return fmt.Errorf("failed to read CSV: %s", err)
+		}
+		headers := removeElements(header, *latIdx, *lngIdx)
+		headers[len(headers) -1] = *name
+		err = writer.Write(headers)
+		if err == io.ErrClosedPipe {
+			return nil
+		}
+		if err != nil {
+			return err
 		}
 	}
 	for {
@@ -53,35 +78,34 @@ func main() {
 			break
 		}
 		if err != nil {
-			log.Fatalf("failed to read CSV: %s\n", err)
+			return fmt.Errorf("failed to read CSV: %s", err)
 		}
 		lat, lng := parseFloat(rec[*latIdx], row), parseFloat(rec[*lngIdx], row)
 		p := orb.Point{lng, lat}
 		dst := make([]string, len(rec)-1)
 		var i int
+		// Exclude lat/lng rows from CSV
 		for j, s := range rec {
 			if j != *latIdx && j != *lngIdx {
 				dst[i] = s
 				i++
 			}
 		}
+		// Append wkt POINT geometry to end of csv
 		dst[len(dst)-1] = wkt.MarshalString(p)
 		err = writer.Write(dst)
 		if err == io.ErrClosedPipe {
 			break
 		}
 		if err != nil {
-			log.Fatalf("unable to write to csv: %v\n", err)
+			return fmt.Errorf("unable to write to csv: %v", err)
 		}
 	}
 	writer.Flush()
 	if err := writer.Error(); err != nil {
-		log.Fatalf("failed to close csv: %v\n", err)
+		return fmt.Errorf("failed to close csv: %v", err)
 	}
-}
-
-func logParseFloatErr(val string, row int, err error) {
-	log.Fatalf("failed to convert value %s to a float, row %d: %v\n", val, row, err)
+	return nil
 }
 
 func parseFloat(val string, row int) float64 {
@@ -90,7 +114,29 @@ func parseFloat(val string, row int) float64 {
 	}
 	f, err := strconv.ParseFloat(val, 64)
 	if err != nil {
-		logParseFloatErr(val, row, err)
+		log.Fatalf("failed to convert value %s to a float, row %d: %v\n", val, row, err)
 	}
 	return f
+}
+
+func removeElements(str []string, one, two int) []string {
+	f := make([]string, len(str) -1)
+	var i int
+	for j := 0; j < len(str); j++ {
+		if j != one && j != two {
+			f[i] = str[j]
+			i++
+		}
+	}
+	return append(f, "wkt")
+}
+
+func closeOnEnd(r io.ReadCloser) {
+	sig := make(chan os.Signal, 1)
+	signal.Notify(sig, syscall.SIGKILL, syscall.SIGINT)
+	<-sig
+	err := r.Close()
+	if err != nil {
+		log.Fatal(err)
+	}
 }
--- main_test.go
+++ main_test.go
@@ -2,21 +2,6 @@ package main
 
 import "testing"
 
-func TestReplace(t *testing.T) {
-	vals := []string{"0", "1", "2", "3", "4"}
-	one := 1
-	two := 3
-	dst := make([]string, len(vals)-1)
-	var i int
-	for j, s := range vals {
-		if j != one && j != two {
-			dst[i] = s
-			i++
-		}
-	}
-	for i, s := range []string{"0", "2", "4"} {
-		if dst[i] != s {
-			t.Errorf("%q should match %q", dst[i], s)
-		}
-	}
+func TestRun(t *testing.T) {
+
 }
